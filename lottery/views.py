cdc2427a8b25384b4b7f52aed478e50083e3f980
Brian Nganga <briannganga70@gmail.com>
2023-01-10 18:01:25 +0300

    purchase ticket

--- lottery/views.py
+++ lottery/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import LotteryTicket
+from django.http import HttpResponse
 
-# Create your views here.
+def purchase_ticket(request):
+    phone_number = request.GET.get('phone_number')
+    ticket_price = 50
+
+    # code to initiate mpesa transaction and handle the response
+    # ...
+
+    lottery_ticket = LotteryTicket.objects.create(
+        player_phone_number=phone_number
+    )
+    return HttpResponse(f'Ticket purchased for {phone_number}')
