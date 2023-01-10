e60e3ce09995fef3a34ec6d91798f642f6e182ae
Brian Nganga <briannganga70@gmail.com>
2023-01-10 21:08:33 +0300

    discard_previous_tickets

--- lottery/views.py
+++ lottery/views.py
@@ -15,7 +15,6 @@ def initiate_stk_push(phone_number, amount):
     stk_response = mpesa.stk_push(amount, phone_number, "Payment for Lottery Ticket")
     return stk_response
 
-
 def purchase_ticket(request):
     phone_number = request.GET.get('phone_number')
     ticket_price = 50
@@ -34,8 +33,11 @@ def purchase_ticket(request):
         # Transaction failed
         return HttpResponse(f'Transaction failed with code: {stk_response["response_code"]}')
 
+def discard_previous_tickets():
+    LotteryTicket.objects.filter(discarded=False).update(discarded=True)
+
 def pick_winner():
-    purchased_tickets = LotteryTicket.objects.all()
+    purchased_tickets = LotteryTicket.objects.filter(discarded=False)
     if purchased_tickets.count() > 0:
         winner = random.choice(purchased_tickets)
         return winner.player_phone_number
@@ -44,9 +46,10 @@ def pick_winner():
 
 def daily_draw(request):
     winner = pick_winner()
-    total_collected = LotteryTicket.objects.count() * 50
-    amount_to_be_won = total_collected * 0.3
     if winner:
+        total_collected = LotteryTicket.objects.filter(discarded=False).count() * 50
+        amount_to_be_won = total_collected * 0.3
+        discard_previous_tickets()
         return HttpResponse(f'The winner of the daily draw is {winner}, winning ksh{amount_to_be_won}')
     else:
         return HttpResponse(f'No tickets were sold for today\'s draw')
