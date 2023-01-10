a850ae006bbd4bfb964eb80520990bdec91c3e18
Brian Nganga <briannganga70@gmail.com>
2023-01-10 20:51:14 +0300

    discardoldtickets

--- lottery/models.py
+++ lottery/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class LotteryTicket(models.Model):
     player_phone_number = models.CharField(max_length=15)
     purchase_date = models.DateTimeField(auto_now_add=True)
+    discarded = models.BooleanField(default=False)
 
     def __str__(self):
         return self.player_phone_number
