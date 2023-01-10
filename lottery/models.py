7f8ebca84e50ce5281a6e0c90f043dc8995e4d42
Brian Nganga <briannganga70@gmail.com>
2023-01-10 17:52:17 +0300

    Initial commit

---
+++ lottery/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class LotteryTicket(models.Model):
+    player_phone_number = models.CharField(max_length=15)
+    purchase_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.player_phone_number
