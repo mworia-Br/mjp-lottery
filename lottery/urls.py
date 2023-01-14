14b606048c572343db0a18fa5b8e48afb0d3ad34
Brian Nganga <briannganga70@gmail.com>
2023-01-15 00:35:27 +0300

    urls

--- lottery/urls.py
+++ lottery/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
-from .views import *
+from django.urls import path, include
+#from .views import *
+from . import views
 
 urlpatterns = [
-    path('daily_draw/', daily_draw_view, name='daily_draw'),
+    path('daily_draw/', views.daily_draw_view, name='daily_draw'),
     # other URL patterns
-    path('stk-push-callback/', stk_push_callback, name='stk_push_callback'),
+    path('callback/', views.mpesa_callback, name='mpesa_callback'),
+    path('api-auth/', include('rest_framework.urls')),
 ]
