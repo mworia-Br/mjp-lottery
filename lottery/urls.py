7cdba94ffd42294978226a24b7e0aac9c0e9378a
Brian Nganga <briannganga70@gmail.com>
2023-01-14 19:16:05 +0300

    mpesa

--- lottery/urls.py
+++ lottery/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
     path('daily_draw/', daily_draw_view, name='daily_draw'),
     # other URL patterns
+    Patth('daraja/stk_push', views.stk_push_callback,name='stk_push_callback'),
 ]
