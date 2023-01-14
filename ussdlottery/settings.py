1d3531db66f6de09dd76671476fea9c3f372e5fb
Brian Nganga <briannganga70@gmail.com>
2023-01-14 16:14:31 +0300

    miggrate

--- ussdlottery/settings.py
+++ ussdlottery/settings.py
@@ -9,15 +9,16 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-# settings.py
-
 import os
 from celery import Celery
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# the load_dotenv function gets the environment variables defined in .env file
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lottery',
 ]
 
 MIDDLEWARE = [
