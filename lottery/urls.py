3e59c54965232d42562ab4b1e99c93db94cfe52e
Brian Nganga <briannganga70@gmail.com>
2023-01-14 19:45:26 +0300

    working

--- lottery/urls.py
+++ lottery/urls.py
@@ -4,5 +4,4 @@ from .views import *
 urlpatterns = [
     path('daily_draw/', daily_draw_view, name='daily_draw'),
     # other URL patterns
-    Patth('daraja/stk_push', views.stk_push_callback,name='stk_push_callback'),
 ]
