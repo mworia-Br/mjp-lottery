b2960a19ddcb8f26562ca6026b51bd4253778d85
Brian Nganga <briannganga70@gmail.com>
2023-01-14 16:17:19 +0300

    register models

--- lottery/admin.py
+++ lottery/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import LotteryTicket, LotteryWinner
+
+admin.site.register(LotteryTicket)
+admin.site.register(LotteryWinner)
